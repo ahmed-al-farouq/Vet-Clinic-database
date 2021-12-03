eacffb613b734fcdccecb1e01a2a9a875dfdbea2
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-12-03 15:36:31 +0200

    add tables, modify them, and add queries

--- data.sql
+++ data.sql
@@ -10,3 +10,56 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-06-12', 1, true, -45.00);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7, true, 20.40);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17.00);
+
+-- Insert into owners table
+INSERT INTO owners (full_name, age) 
+  VALUES ('Sam Smith', 34),
+        ('Jennifer', 19),
+        ('Bob', 45),
+        ('Melody Pond', 77),
+        ('Dean Winchester', 14),
+        ('Jodie Whittaker', 38);
+
+-- Insert into species table
+INSERT INTO species (name)
+  VALUES ('Pokemon'),
+        ('Digimon');
+
+-- set species-id for name ends in "mon"
+BEGIN;
+UPDATE animals
+SET species_id = 1 WHERE name LIKE '%mon';
+COMMIT;
+
+-- set poke
+BEGIN;
+UPDATE animals
+SET species_id = 2 WHERE species_id is NULL;
+COMMIT;
+
+
+-- Modify your inserted animals to include owner information (owner_id):
+BEGIN;
+UPDATE animals
+SET owners_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')  WHERE name='Agumon';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET owners_id = (SELECT id FROM owners WHERE full_name='Jennifer Orwell') WHERE name='Gabumon' OR name='Pikachu';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET owners_id= (SELECT id FROM owners WHERE full_name='Bob') WHERE name='Devimon' OR name='Plantmon';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET owners_id= (SELECT id FROM owners WHERE full_name='Melody Pond') WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET owners_id= (SELECT id FROM owners WHERE full_name='Dean Winchester') WHERE name='Angemon' OR name='Boarmon';
+COMMIT;
